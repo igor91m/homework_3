532fc71e50f12a5cdee26557962e07f35a723188
igor91m <igor91m@yandex.ru>
2021-01-23 01:32:42 +0300

    add changes

--- task_2.py
+++ task_2.py
@@ -5,3 +5,12 @@
 Реализовать вывод данных о пользователе одной строкой.
 '''
 
+# Функция информации о пользователе
+def my_func(name, surname, birth_year, city, email, telephone):
+    return ' '.join([name, surname, birth_year, city, email, telephone])
+print(my_func(name='Vasia',
+              surname='Poopkin,',
+              birth_year='Birth year: 1980,',
+              city='City: Moscow,',
+              email='email: why@mail.ru,',
+              telephone='Telephone: 8(985)532-54-11'))
